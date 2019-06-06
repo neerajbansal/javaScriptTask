560be2baf6c5515e35a488445a91bbfb34b95a72
Neeraj Bansal <neeraj.bansal@stylumia.com>
2019-06-07 01:39:00 +0530

    Logical & Numbers part done

--- app/logicalOperators.js
+++ app/logicalOperators.js
@@ -3,11 +3,11 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     or : function(a, b) {
-        
+      return (a || b ? true : false)
     },
 
     and : function(a, b) {
-
+      return (a && b ? true : false)
     }
   };
 });
--- app/numbers.js
+++ app/numbers.js
@@ -3,19 +3,27 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     valueAtBit: function(num, bit) {
-
+      let binary = num.toString(2);
+      return Number(binary[binary.length - bit]);
     },
 
     base10: function(str) {
-
+      return parseInt(str, 2);
     },
 
     convertToBinary: function(num) {
-
+      let binary = num.toString(2);
+      //Below code adds 0 in front of string to complete 8 digit
+      for(let count = binary.length; count < 8; count++){
+        binary = '0' + binary;
+      }
+      return binary;
     },
 
     multiply: function(a, b) {
-
+      //Precision is unknown as it varies, So calculating precision required
+      let precision = (String(b).split('.')[1]).length || 1;
+      return +(a * b).toFixed(precision);
     }
   };
 });
