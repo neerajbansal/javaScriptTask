6436bf157e295cf936bf5134ba751e58b9dee440
Neeraj Bansal <neeraj.bansal@stylumia.com>
2019-06-07 01:10:53 +0530

    Objects part done

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,20 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+      return fn.call(obj);
     },
 
     alterObjects : function(constructor, greeting) {
-
+      constructor.prototype.greeting = greeting;
     },
 
     iterate : function(obj) {
-
+      let result = [];
+      let keysArr = Object.getOwnPropertyNames(obj);
+      for(let index in keysArr){
+        result.push(keysArr[index] + ": " + obj[keysArr[index]])
+      }
+      return result;
     }
   };
 });
