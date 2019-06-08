cabced9dd8db6b42e57565cb9814686b7e10a55f
Neeraj Bansal <neeraj.bansal@stylumia.com>
2019-06-08 10:32:16 +0530

    Count part is done

--- app/count.js
+++ app/count.js
@@ -3,6 +3,22 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
+      let intervalFun;
+
+      function exe(){
+        console.log(start++)
+        if(start <= end){
+          intervalFun = setTimeout(exe, 100); 
+        }
+        // start++;
+      }
+      exe();
+
+      return {
+        cancel: ()=>{
+          intervalFun && clearTimeout(intervalFun) 
+        }
+      }
     }
   };
 });
