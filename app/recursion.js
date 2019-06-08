0b420b9dbd31824514c3da104c2caf572ed081c2
Neeraj Bansal <neeraj.bansal@stylumia.com>
2019-06-08 10:50:54 +0530

    Recursion  part partially done

--- app/recursion.js
+++ app/recursion.js
@@ -1,13 +1,47 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
-define(function() {
+define(function () {
   return {
-    listFiles: function(data, dirName) {
+    listFiles: function (data, dirName) {
+      let result = []
+      function listing(dir) {
+        if (dir.hasOwnProperty('files')) {
+          for (let index in dir['files']) {
+            let type = typeof dir['files'][index];
+            let file = dir['files'][index];
+            if (dirName) {
+              if (type == 'string' && dir['dir'] == dirName) {
+                result.push(file)
+              } else if (type == 'object') {
+                listing(file)
+              }
+            } else {
+              if (type == 'string') {
+                result.push(file)
+              } else if (type == 'object') {
+                listing(file)
+              }
+            }
 
+          }
+        } else {
+          return
+        }
+      }
+      listing(data, dirName)
+      return result;
     },
 
-    permute: function(arr) {
-
+    permute: function (arr) {
+      function permutations(arr) {
+        return (arr.length === 1) ? arr :
+          arr.reduce((fix, variable, index) => {
+            let remaining = [...arr];
+            remaining.splice(index, 1);
+            return fix.concat(permutations(remaining).map(a => [].concat(variable, a)));
+          }, []);
+      }
+      return permutations(arr)
     }
   };
 });
