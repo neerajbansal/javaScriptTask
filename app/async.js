c48be967dd8a6a1f5b5ba64d524d726171cc9bd2
Neeraj Bansal <neeraj.bansal@stylumia.com>
2019-06-08 10:34:47 +0530

    Async part partially done

--- app/async.js
+++ app/async.js
@@ -1,13 +1,26 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
-define([ 'jquery' ], function($) {
+define(['jquery'], function ($) {
   return {
-    async : function(value) {
-
+    async: function (value) {
+      let promise = new Promise((resolve, reject) => {
+        resolve(value)
+      })
+      return promise;
     },
 
-    manipulateRemoteData : function(url) {
-
+    manipulateRemoteData: function (url) {
+      let promise = new Promise((resolve, reject) => {
+        let data = $.ajax({ url: url });
+        data.done((res) => {
+          let result = [];
+          res["people"].forEach(element => {
+            result.push(element["name"]);
+          });
+          resolve(result);
+        })
+      })
+      return promise;
     }
   };
 });
